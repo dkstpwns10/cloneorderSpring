2932bbb53d7b179a41326bb607547cb9ca2b1fe7
unknown <dkstpwns10@naver.com>
2025-01-28 20:44:38 +0900

    fix

--- src/main/java/com/api/model/Store.java
+++ src/main/java/com/api/model/Store.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
